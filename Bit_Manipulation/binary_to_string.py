dd5c1eae8573c86eef8314789e8d963fa9911b85
Milind Shah <personalmilind@gmail.com>
2017-07-14 15:33:26 -0400

    Finished binary_to_string problem

---
+++ Bit_Manipulation/binary_to_string.py
@@ -0,0 +1,24 @@
+''' Binary to String: Given a real number between 8 and 1 (e.g., 0.72) that is passed in as a double,
+print the binary representation. If the number cannot be represented accurately in binary with at
+most 32 characters, print "ERROR:' '''
+
+def printBinary(num):
+    if num >= 1 or num <= 0:
+        return "Error"
+    half = 0.5
+    output = "."
+    while num > 0:
+        print "num, ", num
+        if len(output) > 32: # if the length is greater than 32 return error
+            print "Error"
+            return output
+        if num >= half:
+            output += "1"
+            num -= half
+        else:
+            output += "0"
+        half = half/2
+    return output
+
+
+print printBinary(0.32)
